2eda69572cf5c61af976c99da2a5e36450d09103
Alexander Frolov <big_air2409@mail.ru>
2018-09-10 21:58:08 +1200

    Added mock userItems to DB.

--- populate_db.py
+++ populate_db.py
@@ -1,9 +1,4 @@
 # Note: TODO - rewrite for catalog item
-# I had to add restaurant_id as python compiler come back with an error 
-# MenuItem does not have prop restaurant. prop restaurant had to be changed to restaurant_id
-# to compile and work properly.
-# Otherwise Works OK
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
  
@@ -43,17 +38,10 @@ session.add(catalogItem02)
 session.commit()
 
 # UserItem1
+userItem1 = UserItem(title = "Shopping in Middle East", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", itemPicture= "../img/pexel-photo-160107.jpeg", catalogItemId = catalogItem01.catalogItemId)
+session.add(userItem1)
 
 # UserItem2
-menuItem2 = MenuItem(name = "Veggie Burger", description = "Juicy grilled veggie patty with tomato mayo and lettuce", price = "$7.50", course = "Entree", restaurant_id = restaurant1.id)
-
-session.add(menuItem2)
+userItem2 = UserItem(title = "Coders Life Before and After", description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." , itemPicture= "../img/pexel-photo-1394841.jpeg", catalogItemId = catalogItem02.catalogItemId)
+session.add(userItem2)
 session.commit()
-
-
-menuItem1 = MenuItem(name = "French Fries", description = "with garlic and parmesan", price = "$2.99", course = "Appetizer", restaurant_id = restaurant1.id)
-
-session.add(menuItem1)
-session.commit()
-
-
